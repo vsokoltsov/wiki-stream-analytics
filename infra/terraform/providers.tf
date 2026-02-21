cd5adcd745dddce31eed85b695aac2b415331834
Vadim Sokoltsov <vforvad@gmail.com>
2026-02-21 11:38:53 +0100

    Add kubectl provider

--- infra/terraform/providers.tf
+++ infra/terraform/providers.tf
@@ -46,6 +46,13 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(google_container_cluster.gke.master_auth[0].cluster_ca_certificate)
 }
 
+provider "kubectl" {
+  host                   = "https://${google_container_cluster.gke.endpoint}"
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(google_container_cluster.gke.master_auth[0].cluster_ca_certificate)
+  load_config_file       = false
+}
+
 provider "helm" {
   kubernetes = {
     host                   = "https://${google_container_cluster.gke.endpoint}"
