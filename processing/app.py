3efba709e3bdda7a706fea2dd582bb8cf738977e
Vadim Sokoltsov <vforvad@gmail.com>
2026-02-21 22:42:24 +0100

    Add pipline to main app

--- processing/app.py
+++ processing/app.py
@@ -12,6 +12,7 @@ from pyflink.common.watermark_strategy import WatermarkStrategy
 from pyflink.table import StreamTableEnvironment, Schema, DataTypes, TableDescriptor
 from pyflink.table.expressions import col
 from processing.settings import get_processing_settings
+from processing.pipeline import DatalakeStreamingPipeline
 
 
 def _dt_hour_from_ts_seconds(ts_seconds: int) -> tuple[str, str]:
@@ -96,77 +97,22 @@ def main():
         )
         .filter(lambda r: r is not None)
     )
+    descriptor_options = {
+        "sink.rolling-policy.file-size": "128MB",
+        "sink.rolling-policy.rollover-interval": "5 min",
+        "sink.partition-commit.trigger": "process-time",
+        "sink.partition-commit.delay": "1 min",
+        "sink.partition-commit.policy.kind": "success-file"
+    }
 
-    # DataStream -> Table
-    curated_table = t_env.from_data_stream(
-        curated_stream,
-        Schema.new_builder()
-        .column_by_expression("event_ts_seconds", col("f0"))
-        .column_by_expression("wiki", col("f1"))
-        .column_by_expression("type", col("f2"))
-        .column_by_expression("user_name", col("f3"))
-        .column_by_expression("bot", col("f4"))
-        .column_by_expression("title", col("f5"))
-        .column_by_expression("namespace_id", col("f6"))
-        .column_by_expression("dt", col("f7"))
-        .column_by_expression("hour", col("f8"))
-        .build()
+    pipeline = DatalakeStreamingPipeline(
+        curated_stream=curated_stream,
+        t_env=t_env,
+        bucket_path=bucket_path,
+        descriptor_options=descriptor_options
     )
 
-    t_env.create_temporary_view("curated_view", curated_table)
-    
-    # Sink table (filesystem -> parquet -> GCS)
-    sink_descriptor = (
-        TableDescriptor.for_connector("filesystem")
-        .schema(
-            Schema.new_builder()
-            .column("event_ts", DataTypes.TIMESTAMP_LTZ(3))
-            .column("wiki", DataTypes.STRING())
-            .column("type", DataTypes.STRING())
-            .column("user_name", DataTypes.STRING())
-            .column("bot", DataTypes.BOOLEAN())
-            .column("title", DataTypes.STRING())
-            .column("namespace_id", DataTypes.INT())
-            .column("dt", DataTypes.STRING())
-            .column("hour", DataTypes.STRING())
-            .build()
-        )
-        .partitioned_by("dt", "hour")
-        .option("path", bucket_path)
-        .format("parquet")
-        # .option("partition.fields", "dt,hour")
-
-        .option("sink.rolling-policy.file-size", "128MB")
-        .option("sink.rolling-policy.rollover-interval", "5 min")
-        .option("sink.rolling-policy.check-interval", "1 min")
-
-        .option("sink.partition-commit.trigger", "process-time")
-        .option("sink.partition-commit.delay", "1 min")
-        .option("sink.partition-commit.policy.kind", "success-file")
-        .build()
-    )
-
-    event_ts_expr = call_sql(
-        "TO_TIMESTAMP_LTZ(CAST(event_ts_seconds * 1000 AS BIGINT), 3)"
-    )
-
-    t_env.create_table("gcs_sink", sink_descriptor)
-
-    table_result = (
-        t_env.from_path("curated_view")
-        .select(
-            event_ts_expr.alias("event_ts"),
-            col("wiki"),
-            col("type"),
-            col("user_name"),
-            col("bot"),
-            col("title"),
-            col("namespace_id"),
-            col("dt"),
-            col("hour"),
-        )
-        .execute_insert("gcs_sink")
-    )
+    table_result = pipeline.build()
 
     table_result.wait()
 
