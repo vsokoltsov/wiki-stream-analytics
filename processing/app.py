e36a11647e58ac1144e17e43c660c677ff7faf67
Vadim Sokoltsov <vforvad@gmail.com>
2026-02-25 23:32:09 +0100

    Remove file extension option

--- processing/app.py
+++ processing/app.py
@@ -122,7 +122,6 @@ def main():
         "sink.partition-commit.trigger": "process-time",
         "sink.partition-commit.delay": "0 s",
         "sink.partition-commit.policy.kind": "success-file",
-        "sink.file-name.suffix": ".parquet",
     }
 
     pipeline = DatalakeStreamingPipeline(
