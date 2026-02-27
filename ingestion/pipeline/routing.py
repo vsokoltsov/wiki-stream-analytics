0a4fdd9aabc37200a6f8296643f3a0259a9c2aaf
Vadim Sokoltsov <vforvad@gmail.com>
2026-02-27 22:19:43 +0100

    Add routing step for pipeline

---
+++ ingestion/pipeline/routing.py
@@ -0,0 +1,16 @@
+from typing import Dict, Any
+import re
+
+DT_RE = re.compile(r"dt=(\d{4}-\d{2}-\d{2})")
+HR_RE = re.compile(r"hour=(\d{1,2})")
+
+def add_partition_info(e, table_id: str):
+    prefix = e["prefix"]
+    m_dt = DT_RE.search(prefix)
+    m_hr = HR_RE.search(prefix)
+    if m_dt:
+        e["dt"] = m_dt.group(1)
+    if m_hr:
+        e["hour"] = int(m_hr.group(1))
+    e["bq_table"] = table_id
+    return e
