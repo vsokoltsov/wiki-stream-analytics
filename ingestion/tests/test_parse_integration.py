1914154ab72c5139ecacde5f74bbc111651745fd
Vadim Sokoltsov <vforvad@gmail.com>
2026-02-27 22:13:12 +0100

    Group parse tests by type

--- ingestion/tests/test_parse_integration.py
+++ ingestion/tests/test_parse_integration.py
@@ -5,41 +5,44 @@ from apache_beam.testing.util import assert_that, equal_to
 
 from ingestion.pipeline.parse import parse_notification, is_final_object 
 
-def test_beam_pipeline_parse_and_filter_final_objects():
-    messages = [
-        PubsubMessage(b"", {"bucketId": "b", "objectId": "stream/a/part-1"}),
-        PubsubMessage(b"", {"bucketId": "b", "objectId": ".inprogress/stream/a/part-2"}),
-        PubsubMessage(b"", {"bucketId": "b", "objectId": ".pending/stream/a/part-3"}),
-        PubsubMessage(b"", {"bucketId": "b", "objectId": "stream/a/part-4"}),
-    ]
+@pytest.mark.integration
+class TestParseIntegration:
 
-    expected_paths = [
-        "gs://b/stream/a/part-1",
-        "gs://b/stream/a/part-4",
-    ]
+    def test_beam_pipeline_parse_and_filter_final_objects(self):
+        messages = [
+            PubsubMessage(b"", {"bucketId": "b", "objectId": "stream/a/part-1"}),
+            PubsubMessage(b"", {"bucketId": "b", "objectId": ".inprogress/stream/a/part-2"}),
+            PubsubMessage(b"", {"bucketId": "b", "objectId": ".pending/stream/a/part-3"}),
+            PubsubMessage(b"", {"bucketId": "b", "objectId": "stream/a/part-4"}),
+        ]
 
-    with beam.Pipeline() as p:
-        out = (
-            p
-            | "Create" >> beam.Create(messages)
-            | "Parse" >> beam.Map(parse_notification)
-            | "FilterFinal" >> beam.Filter(is_final_object)
-            | "ExtractPath" >> beam.Map(lambda e: e["file_path"])
-        )
+        expected_paths = [
+            "gs://b/stream/a/part-1",
+            "gs://b/stream/a/part-4",
+        ]
 
-        assert_that(out, equal_to(expected_paths))
-
-
-def test_beam_pipeline_parse_missing_required_keys_fails():
-    messages = [
-        PubsubMessage(b"", {"bucketId": "b", "objectId": "stream/a/part-1"}),
-        PubsubMessage(b"", {"bucketId": "b"}),  # missing objectId
-    ]
-
-    with pytest.raises(Exception):
         with beam.Pipeline() as p:
-            _ = (
+            out = (
                 p
-                | beam.Create(messages)
-                | beam.Map(parse_notification)
+                | "Create" >> beam.Create(messages)
+                | "Parse" >> beam.Map(parse_notification)
+                | "FilterFinal" >> beam.Filter(is_final_object)
+                | "ExtractPath" >> beam.Map(lambda e: e["file_path"])
             )
+
+            assert_that(out, equal_to(expected_paths))
+
+
+    def test_beam_pipeline_parse_missing_required_keys_fails(self):
+        messages = [
+            PubsubMessage(b"", {"bucketId": "b", "objectId": "stream/a/part-1"}),
+            PubsubMessage(b"", {"bucketId": "b"}),  # missing objectId
+        ]
+
+        with pytest.raises(Exception):
+            with beam.Pipeline() as p:
+                _ = (
+                    p
+                    | beam.Create(messages)
+                    | beam.Map(parse_notification)
+                )
