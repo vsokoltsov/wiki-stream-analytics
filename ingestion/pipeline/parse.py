6347423f8475bca0e86cf7b656e6956787b7aa6d
Vadim Sokoltsov <vforvad@gmail.com>
2026-02-27 21:58:38 +0100

    Add parse pipeline step

---
+++ ingestion/pipeline/parse.py
@@ -0,0 +1,25 @@
+import apache_beam as beam
+from typing import Dict, Any
+from apache_beam.io.gcp.pubsub import PubsubMessage
+
+def parse_notification(m: PubsubMessage) -> Dict[str, Any]:
+    attrs = m.attributes or {}
+    bucket = attrs["bucketId"]
+    obj = attrs["objectId"]
+    gen = attrs.get("objectGeneration", "")
+    prefix = obj.rsplit("/", 1)[0] + "/"
+
+    return {
+        "bucket": bucket,
+        "object": obj,
+        "generation": gen,
+        "event_time": attrs.get("eventTime"),
+        "event_type": attrs.get("eventType"),
+        "file_path": f"gs://{bucket}/{obj}",
+        "prefix": prefix,
+        "attributes": dict(attrs),
+    }
+
+def is_final_object(e: dict) -> bool:
+    obj = e["object"]
+    return not (obj.startswith(".inprogress/") or obj.startswith(".pending/"))
